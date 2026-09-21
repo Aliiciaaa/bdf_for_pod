2c33ee7c54c8d146eb2e32dc3c5f68a3b46eb611
Alicia Garcia-Mascaraque <99747359+Aliiciaaa@users.noreply.github.com>
2026-09-21 14:44:59 +0200

    Clarify and translate comments

--- 00_compute_pod_basis/step2_compute_snapshots_diff.m
+++ 00_compute_pod_basis/step2_compute_snapshots_diff.m
@@ -1,39 +1,34 @@
 % Code for computing finite differences in time.
 clear all; close all;
 
-%Ms = [32,64,128,256,512]; % number of time steps
-Ms = 1024;
+Ms = [32,64,128,256,512,1024]; % number of time steps
 beta = 3; alpha = 1;
 load('snapshots_alpha1_beta3_nu0p002.mat'); % load snapshots for the corresponding beta and alpha
 
 for i = 1 : length(Ms)
 
     M = Ms(i);
-
-    % Extraemos U y V desde UV
     K = 2048/M;
     Zhk = Zh(:,1:K:end); p = mean(Zhk,2);
     
-    % Yh tiene condiciones Dirichlet homogéneas.
-    % anohter option is to take p as
+    % Yh - FEM solution with homogenenous Dirichlet conditions
+    % option 2. take p as
     % nn = size(Zh,1)/2;
     % p1 = ones(nn,1)*alpha; p2 = ones(nn,1)*beta/alpha;
     % p = [p1;p2];
-
-    Yh = Zhk - p;
+    
+    Yh = Zhk - p; % option 1. take p as the mean
     
     dt = tp/M;
     if size(Yh, 2) < 2
-        error('No hay suficientes columnas para calcular la derivada temporal.');
+        error('Unsufficient columns for computing finite differences.');
     end
     Yt = diff(Yh, [], 2) / dt;
     
-    % Construcción de la matriz UN
-    
-    UN = Yt; % we do not consider the initial term
+    UN = Yt; % Note we do not consider the initial term
     
     name = ['snapshots_diff_M',num2str(M),'.mat'];
     save(name);
     
-    disp(['Resultados guardados en el archivo ', name, '.mat']);
+    disp(['Finite differences saved in ', name, '.mat']);
 end
